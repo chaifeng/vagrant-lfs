41e30247ad751ead541b99c6bac378a25b382f10
Chai Feng <chaifeng@chaifeng.com>
2020-09-11 20:09:48 +0800

    init

---
+++ Vagrantfile
@@ -0,0 +1,166 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "bento/ubuntu-18.04"
+
+  lfs_disk_size = 12 # 12G Hard disk for LFS
+
+  lfs_disk_size = 12 if lfs_disk_size < 12
+  lfs_disk = "lfs-disk-#{lfs_disk_size}G.vmdk"
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = false
+
+    vb.memory = "8192"
+    vb.cpus = 2
+
+
+    unless File.exist?(lfs_disk)
+      vb.customize [ "createmedium", "disk", "--filename", lfs_disk,
+                     "--format", "vmdk", "--size", 1024 * lfs_disk_size ]
+    end
+
+    vb.customize [ "storageattach", :id , "--storagectl",
+                   "SATA Controller", "--port", "1", "--device", "0", "--type",
+                   "hdd", "--medium", lfs_disk]
+  end
+
+  config.vm.provision "shell", inline: <<-SHELL
+    set -eu
+
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+    function mk() {
+        for d; do
+            [[ -d "$d" ]] || mkdir -pv "$d"
+        done
+    }
+
+    disk_size=$((#{lfs_disk_size}*1024))
+    sdb1_size=500
+    swap_size=1045
+
+		cat >/sdb.txt <<-SFDISK
+		label: dos
+		device: /dev/sdb
+		unit: sectors
+
+		/dev/sdb1 : start=        2048, size=     $((sdb1_size*2048)), type=83
+		/dev/sdb2 : start=     $(((sdb1_size + 1)*2048)), size=    $(( (disk_size - sdb1_size - 1)*2048 )), type=5
+		/dev/sdb5 : start=     $(( (sdb1_size + 1 + 1)*2048)), size=    $(( (disk_size - swap_size -1 - sdb1_size - 1 - 1)*2048)), type=83
+		/dev/sdb6 : start=    $(((disk_size - 1045)*2048)), size=     $((swap_size*2048)), type=83
+		SFDISK
+
+    if [[ -b /dev/sdb && ! -b /dev/sdb5 ]]; then
+        {
+            echo o
+            echo I
+            echo /sdb.txt
+            echo w
+        } | fdisk /dev/sdb
+        mkfs -v -t ext4 /dev/sdb1
+        mkfs -v -t ext4 /dev/sdb5
+        mkswap /dev/sdb6
+    fi
+
+    cd /vagrant
+    mk lfs
+    cd lfs
+    for file in LFS-BOOK-10.0-NOCHUNKS.html LFS-BOOK-10.0.tar.xz lfs-bootscripts-20200818.tar.xz md5sums wget-list; do
+        echo Fetching $file
+        url="http://www.linuxfromscratch.org/lfs/downloads/10.0/$file"
+        [[ -f "${file}" ]] || curl --remote-name --silent -LSs "$url"
+    done
+
+    mk files
+    pushd files
+    if [[ ! -f .done ]]; then
+       wget --continue -i ../wget-list
+       md5sum --check ../md5sums && touch .done
+    fi
+    popd
+
+    [[ -d 10.0 ]] || tar xf LFS-BOOK-10.0.tar.xz
+    lfs_source=/vagrant/lfs/10.0
+    lfs_files=/vagrant/lfs/files
+
+    /vagrant/chapter.sh
+
+    echo "export LFS=/mnt/lfs" > /etc/profile.d/lfs.sh
+    source /etc/profile.d/lfs.sh
+
+    mk "${LFS}"
+
+    if [[ -b /dev/sdb5 ]]; then
+        mount | grep -F /dev/sdb5 || mount -v -t ext4 /dev/sdb5 "${LFS}"
+    fi
+
+    mk "${LFS}/boot"
+    if [[ -b /dev/sdb1 ]]; then
+        mount | grep -F /dev/sdb1 || mount -v -t ext4 /dev/sdb1 "${LFS}/boot"
+    fi
+
+    if [[ -b /dev/sdb6 ]]; then
+        /sbin/swapon | grep -F /dev/sdb6 || /sbin/swapon /dev/sdb6
+    fi
+
+    mk "$LFS/sources"
+
+    if [[ ! -f "${LFS}/sources/.done" ]]; then
+       cp -rv /vagrant/lfs/md5sums "${lfs_source}" "${lfs_files}"/* "${LFS}/sources"
+       chmod -v a+wt "$LFS/sources"
+       touch "${LFS}/sources/.done"
+    fi
+
+    if [[ ! -e /bin/sh-dash ]]; then
+        mv -v /bin/sh /bin/sh-orig
+        ln -srv /bin/bash /bin/sh
+        mv -v /bin/sh-orig /bin/sh-dash
+    fi
+
+    [ ! -e /etc/bash.bashrc ] || mv -v /etc/bash.bashrc /etc/bash.bashrc.NOUSE
+
+    declare -a packages=()
+    for app in bison gcc-6 g++-6 m4; do
+        hash "$app" || packages+=("$app")
+    done
+    hash makeinfo || packages+=(texinfo)
+
+    if [[ -n "${packages:-}" ]]; then
+        apt update
+        DEBIAN_FRONTEND=noninteractive apt install -y "${packages[@]}"
+    fi
+
+    if ! update-alternatives --query gcc | grep -F /usr/bin/gcc-6; then
+        update-alternatives --remove-all gcc || true
+        update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 50
+    fi
+    if ! update-alternatives --query g++ | grep -F /usr/bin/g++-6; then
+        update-alternatives --remove-all g++ || true
+        update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 50
+    fi
+
+  SHELL
+
+  config.vm.provision "shell", privileged: false, inline: <<-SHELL
+    [[ -f ~/.bash_profile ]] && rm -v ~/.bash_profile
+    echo '
+    set +h
+    umask 022
+    LFS=/mnt/lfs
+    LC_ALL=POSIX
+    LFS_TGT=$(uname -m)-lfs-linux-gnu
+    PATH=/usr/bin
+    if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+    PATH=$LFS/tools/bin:$PATH
+    export LFS LC_ALL LFS_TGT PATH
+		' > ~/.bashrc
+
+    source ~/.bashrc
+    pushd "${LFS}/sources"
+    md5sum --check md5sums
+
+    /vagrant/gen-lfs-script.sh /vagrant/lfs/10.0 > $LFS/sources/lfs.sh
+    ls -l "$LFS/sources/lfs.sh"
+  SHELL
+end
---
+++ gen-lfs-script.sh
@@ -0,0 +1,584 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    cat <<HELP
+Usage: $0 [lfs|chapter02/file.html]
+HELP
+    exit 1
+fi
+
+function script-filter() {
+    local chapter="$1"
+    local result_check_function="$(result-filter "${chapter}")"
+    declare -a make_check_filter=(cat)
+    if [[ -n "$result_check_function" ]]; then
+        echo "$result_check_function"
+        make_check_filter=(sed -E -e "/\\bmake\\b.+\\b(check|test)\\b/s/^(.+)\$/\\( \\1 || true \\) |\\& checking-result/")
+    fi
+    case "${chapter}" in
+        */introduction.html)
+            true
+            ;;
+
+        chapter04/creatingminlayout.html)
+            echo 'if [[ "$UID" -ne 0 ]]; then sudo "$SHELL" -x "$0" "${current_chapter}"; exit; fi'
+            cat
+            ;;
+        chapter04/addinguser.html)
+            echo 'if [[ "$UID" -ne 0 ]]; then sudo "$SHELL" -x "$0" "${current_chapter}"; exit; fi'
+            sed -Ee "/^(su|groupadd|useradd|passwd) /s/^/: /" -e "s/\\blfs\\b/vagrant/g"
+            ;;
+        chapter04/settingenvironment.html)
+            sed -E -e "s/^(source|exec) /: \\1 /"
+            ;;
+
+        chapter0[56]/*)
+            cat
+            ;;
+
+        chapter07/changingowner.html|chapter07/kernfs.html)
+            echo 'if [[ "$UID" -ne 0 ]]; then sudo "$SHELL" -x "$0" "${current_chapter}"; exit; fi'
+            echo 'function mknod() { [[ -e $3 ]] || command mknod "$@"; }'
+            echo 'function mount() { command mount | grep -F "$4" || command mount "$@"; }'
+            cat
+            ;;
+        chapter07/chroot.html|chapter08/pkgmgt.html)
+            true
+            ;;
+        chapter0[789]/*|chapter10/*)
+            echo "
+if [[ \"\${LFS_CHROOT:-}\" != true || ! -d /sources ]]; then
+    sudo /usr/sbin/chroot \"$LFS\" /usr/bin/env -i LFS_CHROOT=true \\
+                       HOME=/root                         \\
+                       TERM=\"\$TERM\"                       \\
+                       PS1='(lfs chroot) \\u:\\w\\\$ '        \\
+                       PATH=/bin:/usr/bin:/sbin:/usr/sbin \\
+                       /bin/bash --login +h -xc \"bash -x /sources/\${0##*/} \${current_chapter}:run\"
+    exit
+fi
+"
+            ;;&
+        chapter07/createfiles.html)
+            sed -e "/^exec /s/^/: /" -e "s/^ln -sv/ln -sfv/"
+            ;;
+        chapter07/stripping.html)
+            echo '[[ -f "$HOME/lfs-temp-tools-10.0.tar.xz" ]] && exit 0'
+            sed -e "/^exit/a); ;;&\n\n(all|chapter07|chapter07/stripping.html|chapter07/stripping.html:non-chroot-run) (\n" \
+                -e "/^strip /s/\$/ || true/" \
+                -e "/^umount /s/^/: /" \
+                -e "/^tar /s/.\$/bin usr tools/" \
+                -e "/^tar /s,^,test -f \"\$HOME\\/lfs-temp-tools-10.0.tar.xz\" || ," \
+                -e "s/^\(strip\|tar\|test\) /sudo \\1 /"
+            ;;
+        chapter07/*)
+            cat
+            ;;
+
+        chapter08/glibc.html)
+            sed -e "/^tzselect/s/^/: /" -e "/^ln .\\+<xxx>/s/<xxx>/Etc\\/UTC/"
+            ;;
+        chapter08/gmp.html)
+            sed -Ee "/^(ABI=32|cp) /s/^/: /"
+            ;;
+        chapter08/shadow.html)
+            sed -e "/^passwd /s/^passwd/passwd --delete/"
+            ;;
+        chapter08/bash.html)
+            sed -Ee '/^exec /s/^/: /'
+            ;;
+        chapter08/autoconf.html)
+            sed "/^make check/s/^/: /"
+            ;;
+        chapter08/groff.html)
+            sed -e "s,<paper_size>,A4,"
+            ;;
+        chapter08/vim.html)
+            sed -e "/^vim -c /s/^/: /"
+            ;;
+        chapter08/util-linux.html)
+            sed -e "/^bash /s/^/: /"
+            ;;
+        chapter08/revisedchroot.html)
+            echo "id tester || exit 0"
+            sed -e "/^logout/s/^/: /" -e "/^chroot /s/^/: /"
+            ;;
+        chapter08/*)
+            cat
+            ;;
+        chapter09/symlinks.html)
+            sed -e "/^udevadm /s/^/: /" \
+                -e "/^bash .\\+init-net-rules\\.sh/s,^,test -f /etc/udev/rules.d/70-persistent-net.rules || ,"
+            ;;
+        chapter09/network.html)
+            sed -e "s/192\\.168\\.1\\.1\$/192.168.56.1/" \
+                -e "s/192\\.168\\.1\\.2\$/192.168.56.200/" \
+                -e "s/192\\.168\\.1\\.255/192.168.56.255/" \
+                -e "/^domain /s/^/# /" \
+                -e "s,<IP address of your primary nameserver>,192.168.56.1," \
+                -e "s,<IP address of your secondary nameserver>,1.1.1.1," \
+                -e "/<192.168.1.1>/s/^/# /" \
+                -e "s,<FQDN>,lfs.local," \
+                -e "s,<HOSTNAME>,lfs," \
+                -e "s,<lfs>,lfs.local,"
+            ;;
+        chapter09/usage.html)
+            sed -e "/^\\(KEYMAP\\|FONT\\|LEGACY\\)/s/^/# /"
+            ;;
+        chapter09/profile.html)
+            sed -e "s/<locale name>/en_US.UTF-8/" -e "s/^\\(export LANG\\)=.\\+/\\1=en_US.UTF-8/"
+            ;;
+        chapter09/*)
+            cat
+            ;;
+        chapter10/fstab.html)
+            for uuid in /dev/disk/by-id/*; do
+                if [[ "$(readlink $uuid)" = */sdb1 ]]; then
+                    sdb1="$uuid"
+                elif [[ "$(readlink $uuid)" = */sdb5 ]]; then
+                    sdb5="$uuid"
+                elif [[ "$(readlink $uuid)" = */sdb6 ]]; then
+                    sdb6="$uuid"
+                fi
+            done
+            sed -e "/<xxx>/a${sdb1} /boot ext4 defaults 1 1" \
+                -e "/<xxx>/s,<xxx>,${sdb5#/dev/}," \
+                -e "/<fff>/s,<fff>,ext4," \
+                -e "/<yyy>/s,<yyy>,${sdb6#/dev/}," \
+                -e "/^hdparm /s/^/# /"
+            ;;
+        chapter10/kernel.html)
+            sed -e "/^make menuconfig/s/menuconfig/defconfig/" \
+                -e "/^mount .\\+\\/boot/s/^/: /"
+            ;;
+        chapter10/grub.html)
+            sed -e "/^cd /,/^xorriso /s,^,: ," \
+                -e "/^set root/s/=.\\+/=(hd0,msdos1)/" \
+                -e "s,sda2,sda5," \
+                -e "/^grub-install/s/sda/sdb/" \
+                -e "s,/boot/vmlinuz,/vmlinuz," \
+                -e "/^menuentry/a\ \ \ \ set root=(hd0,msdos1)"
+
+            ;;
+        chapter10/*)
+            cat
+            ;;
+        *)
+            true
+            ;;
+    esac | "${make_check_filter[@]}"
+}
+
+function result-filter() (
+    local chapter="$1"
+    case "${chapter}" in
+        chapter08/glibc.html)
+            function check() {
+                grep "^FAIL:" |
+                    sed "/\\(io\\/tst-lchmod\\|misc\\/tst-ttyname\\|nss\\/tst-nss-files-hosts-multi\\|rt\\/tst-cputimer[123]\\|tst-mutex10\|sunrpc\\/tst-udp-timeout\\)/d"
+            }
+            ;;
+        chapter08/gcc.html)
+            # Additionally the following tests related to the following files are known to fail with glibc-2.32: asan_test.C, co-ret-17-void-ret-coro.C, pr95519-05-gro.C, pr80166.c.
+            function check() {
+                grep "^FAIL" |
+                    sed -Ee "/(asan_test|co-ret-17-void-ret-coro|pr95519-05-gro|pr80166)\\.[cC]/d" \
+                        -e "/\\/numpunct\\/members\\//d" \
+                        -e "/\\/time_get\\/get_time\\//d"
+            }
+            ;;
+        chapter08/libtool.html)
+            function check() {
+                grep -F '64 failed (59 expected failures).' >/dev/null || echo failed
+            }
+            ;;
+        chapter08/xz.html)
+            # function check() { :; }
+            ;;
+        chapter08/zstd.html)
+            # function check() { :; }
+            ;;
+        chapter08/file.html)
+            # function check() { :; }
+            ;;
+        chapter08/readline.html)
+            # function check() { :; }
+            ;;
+        chapter08/m4.html)
+            # function check() { :; }
+            ;;
+        chapter08/bc.html)
+            # function check() { :; }
+            ;;
+        chapter08/flex.html)
+            # function check() { :; }
+            ;;
+        chapter08/binutils.html)
+            # function check() { :; }
+            ;;
+        chapter08/gmp.html)
+            # function check() { :; }
+            ;;
+        chapter08/gperf.html)
+            # function check() { :; }
+            ;;
+        chapter08/expat.html)
+            # function check() { :; }
+            ;;
+        chapter08/inetutils.html)
+            # function check() { :; }
+            ;;
+        chapter08/perl.html)
+            # function check() { :; }
+            ;;
+        chapter08/xml-parser.html)
+            # function check() { :; }
+            ;;
+        chapter08/intltool.html)
+            # function check() { :; }
+            ;;
+        chapter08/autoconf.html)
+            # function check() { :; }
+            ;;
+        chapter08/automake.html)
+            # function check() { :; }
+            ;;
+        chapter08/kmod.html)
+            # function check() { :; }
+            ;;
+        chapter08/libelf.html)
+            # function check() { :; }
+            ;;
+        chapter08/libffi.html)
+            # function check() { :; }
+            ;;
+        chapter08/openssl.html)
+            # function check() {
+            #     grep "^FAIL" "$log" |
+            #         sed -ne "/^Test Summary Report/,/^  Failed test:/p" | sed -e 1,2d -e '$d' | sed -e "/30-test_afalg\\.t/d"
+            # }
+            ;;
+        chapter08/Python.html)
+            # function check() { :; }
+            ;;
+        chapter08/ninja.html)
+            # function check() { :; }
+            ;;
+        chapter08/meson.html)
+            # function check() { :; }
+            ;;
+        chapter08/coreutils.html)
+            # function check() { :; }
+            ;;
+        chapter08/check.html)
+            # function check() { :; }
+            ;;
+        chapter08/diffutils.html)
+            # function check() { :; }
+            ;;
+        chapter08/gawk.html)
+            # function check() { :; }
+            ;;
+        chapter08/findutils.html)
+            # function check() { :; }
+            ;;
+        chapter08/groff.html)
+            # function check() { :; }
+            ;;
+        chapter08/grub.html)
+            # function check() { :; }
+            ;;
+        chapter08/less.html)
+            # function check() { :; }
+            ;;
+        chapter08/gzip.html)
+            # function check() { :; }
+            ;;
+        chapter08/iproute2.html)
+            # function check() { :; }
+            ;;
+        chapter08/kbd.html)
+            # function check() { :; }
+            ;;
+        chapter08/libpipeline.html)
+            # function check() { :; }
+            ;;
+        chapter08/make.html)
+            # function check() { :; }
+            ;;
+        chapter08/patch.html)
+            # function check() { :; }
+            ;;
+        chapter08/man-db.html)
+            # function check() { :; }
+            ;;
+        chapter08/tar.html)
+            function check() {
+                grep -F FAILED |
+                    sed "/store\\/restore/d"
+            }
+            ;;
+        chapter08/texinfo.html)
+            # function check() { :; }
+            ;;
+        chapter08/vim.html)
+            # function check() { :; }
+            ;;
+        chapter08/eudev.html)
+            # function check() { :; }
+            ;;
+        chapter08/procps-ng.html)
+            # function check() { :; }
+            ;;
+        chapter08/util-linux.html)
+            # function check() { :; }
+            ;;
+        chapter08/e2fsprogs.html)
+            # function check() { :; }
+            ;;
+        chapter08/sysklogd.html)
+            # function check() { :; }
+            ;;
+        chapter08/sysvinit.html)
+            # function check() { :; }
+            ;;
+        chapter08/aboutdebug.html)
+            # function check() { :; }
+            ;;
+        chapter08/strippingagain.html)
+            # function check() { :; }
+            ;;
+        chapter08/revisedchroot.html)
+            # function check() { :; }
+            ;;
+        *)
+            # function check() { :; }
+            ;;
+    esac
+
+    if [[ function = "$(type -t check)" ]]; then
+        declare -pf check
+    fi
+)
+
+function unhtml() {
+    sed -e "s/<[^>]\\+>//g" -e "s/^<[^>]\\+\$//" -e "s/^[^>]\\+>//" \
+        -e 's/\&gt;/>/g' -e 's/\&lt;/</g' -e 's/\&nbsp;/ /g' \
+        -e 's/\&amp;/\&/g' \
+        -e 's/^[[:blank:]\+]$//'
+}
+
+function lfs-code-header() {
+    cat <<\FUNCTIONS
+#!/bin/bash
+set -euo pipefail
+
+function clean() {
+    local build_dir
+    for dir; do
+        build_dir="${LFS:-}/sources/${dir##*/}"
+        [[ -d "$build_dir" ]] && rm -rf "$build_dir"
+    done
+}
+
+function xf() {
+    local archive="${LFS:-}/sources/${1##*/}"
+    local dir="${LFS:-}/sources/${2##*/}"
+    [[ -d "$dir" ]] || mkdir -v "$dir"
+    tar --strip-components=1 -C "${dir}" -xf "$archive"
+    cd "$dir"
+}
+
+function checking-result() {
+    local log="lfs-check.log"
+    tee "$log"
+    local result="$(cat "$log" | check)"
+    [[ -z "$result" ]]
+}
+
+trap 'echo "${current_chapter}"' EXIT
+
+FUNCTIONS
+
+    cat <<\SCRIPT
+
+## Build Linux From Scratch
+chapter="${1:-}"
+chapter="${chapter:+chapter}${chapter#*chapter}"
+chapter="${chapter%/}"
+echo "${chapter:=all}"
+
+for p in ~vagrant/.bash_profile ~vagrant/.bashrc; do
+    [[ -f "$p" ]] && source "$p"
+done
+
+export LFS="${LFS:-}"
+cd "${LFS}/sources"
+
+case "${chapter}" in
+
+SCRIPT
+}
+
+function lfs-code-footer() {
+    cat <<\SCRIPT
+
+## DONE
+
+(*)
+    current_chapter="All done."
+;;
+esac
+
+echo "===== DONE ======"
+
+SCRIPT
+}
+
+function lfs-code() (
+    local source="${1}"
+    local chapter="chapter${source#*/chapter}"
+
+    local script title chapter_number
+    script="$(sed -n "/<pre .\\+\\(userinput\\|install\\|root\\)/,/<\\/pre>/p" "$@" |
+        sed -Ee "/<[^>]+\$/N;s/\\n/ /" | sed -Ee "/<[^>]+\$/N;s/\\n/ /" | sed -Ee "/<[^>]+\$/N;s/\\n/ /" |
+        unhtml |
+        script-filter "${chapter}")"
+    [[ -n "${script}" ]] || return 0
+
+    title="$(sed -n "/<title>/,/<\\/title>/p" "${source}" |
+        sed -n 2p |
+        sed "s/^ \\+//" |
+        unhtml)"
+    chapter_number="$(printf "%d%02d" "$(cut -d. -f1 <<< "$title")" "$(cut -d. -f2 <<< "$title")")"
+
+    local build_dir build_done
+    build_dir="${chapter_number}-${chapter#*/}"
+    build_dir="${build_dir%.*}.build"
+    #build_done="${chapter//\//_}"
+    build_done="${build_dir%.*}.done"
+
+    cat <<TITLE
+
+
+###################################################################################################
+######## ------------------------- $title ---------------------
+######## $chapter
+
+(all|${chapter%/*}|${chapter}|${chapter}:*)
+    current_chapter="${chapter}"; echo "\$current_chapter"
+    build_done="\${LFS:-}/sources/${build_done}"
+    echo -e "\e[31m${chapter}\e[0m"
+    (
+    cd "\${LFS:-}/sources"
+    if [[ -f "\${build_done}" ]]; then
+        echo -e "\e[33mSkipping ${chapter}\e[0m"
+        exit
+    fi
+
+    case "\$chapter" in
+
+TITLE
+    shopt -s nullglob
+
+    declare -a file=()
+    filename="${chapter##*/}"
+    until [[ "${filename}" = "${filename%[.-]*}" ]]; do
+        filename="${filename%[.-]*}"
+        file=("${LFS}/sources/${filename}"*.{bz2,gz,xz,tgz})
+        case "$filename" in
+            (tcl)
+                file=("${LFS}/sources/${filename}"*-src*.{bz2,gz,xz,tgz})
+                ;;
+            (xml-parser)
+                file=("${LFS}/sources/XML-Parser-"*.{bz2,gz,xz,tgz})
+                ;;
+            (libelf)
+                file=("${LFS}/sources/elfutils"*.{bz2,gz,xz,tgz})
+                ;;
+            (bootscripts)
+                file=("${LFS}/sources/lfs-${filename}"*.{bz2,gz,xz,tgz})
+                ;;
+            (kernel)
+                file=("${LFS}/sources/linux-"*.{bz2,gz,xz,tgz})
+                ;;
+        esac
+        if [[ "${#file[@]}" -eq 1 ]]; then
+            break
+        elif [[ "${#file[@]}" -gt 1 ]]; then
+            echo "exit 2 # $chapter ::: " "${file[@]}" >&2
+            exit 2
+        fi
+    done
+
+    if [[ -f "$file" ]]; then
+        cat <<SCRIPT
+    (all|${chapter%/*}|${chapter}|${chapter}:clean)
+    echo -e "\e[31m${chapter}:clean\e[0m"
+    [[ -d "${build_dir}" ]] && sudo rm -rf "${build_dir}"
+    ;;&
+    (all|${chapter%/*}|${chapter}|${chapter}:run|${chapter}:extract)
+    echo -e "\e[31m${chapter}:extract\e[0m"
+    xf "${file##*/}" "${build_dir}"
+    ;;&
+SCRIPT
+    fi
+    cat <<SCRIPT
+
+    (all|${chapter%/*}|${chapter}|${chapter}:run) (
+    echo -e "\e[31m${chapter}:run \$(pwd)\e[0m"
+    set -x
+# ---------------------------------------------
+
+${script}
+
+# ---------------------------------------------
+    cd \${LFS:-}/sources
+    if [[ -n "${build_dir}" ]] && [[ -d "${build_dir}" ]]; then
+        rm -rf "${build_dir}" || sudo rm -rf "${build_dir}"
+    fi
+    [[ -z "$file" ]] || touch "\${build_done}"
+    ); ;;
+    esac
+
+); ;;& ##### =========== ${chapter} ===================================
+SCRIPT
+)
+
+function lfs-chapter-code() {
+    local chapter="$1"
+    cat "${chapter}" |
+        sed -n "/Table of Content/,/<\\/ul>/p" |
+        grep -Eo "[^\"]+\\.html" |
+        sed "/introduction\\.html/d" |
+        while read -r html; do
+            lfs-code "${chapter%/*}/$html"
+        done
+}
+
+source="${1%/}"
+if [[ ! -e "$source" ]]; then
+    echo "$source does not exist." >&2
+    exit 1
+fi
+source="$(realpath "$source")"
+chapter="chapter${source#*/chapter}"
+
+if [[ -d "$source" && -d "${source}/chapter01" && -f "${source}/chapter01/chapter01.html" ]]; then
+    lfs-code-header
+    echo "## All files"
+    for chapter in "${source}"/chapter??; do
+        lfs-chapter-code "${chapter}/${chapter##*/}.html"
+    done
+    lfs-code-footer
+elif [[ -d "$source" && -f "${source}/${source##*/}.html" ]]; then
+    lfs-code-header
+    lfs-chapter-code "${source}/${source##*/}.html"
+    lfs-code-footer
+elif [[ -f "$source" && "${chapter}" = chapter??/*.html ]]; then
+    lfs-code-header
+    lfs-code "$source"
+    lfs-code-footer
+else
+    echo "Unknown $source."
+    exit 1
+fi
